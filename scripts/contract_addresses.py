20ef5771480313b2a0595afce0335948c6155edd
apguerrera <adrianguerrera@gmail.com>
2021-03-29 03:06:25 -0700

    Added contract addresses

--- scripts/contract_addresses.py
+++ scripts/contract_addresses.py
@@ -1,12 +1,12 @@
 CONTRACTS = {
     "ropsten":{
-         "access_control":"",
-         "gaze_coin": "",
-         "weth":"",
-         "lp_token":"",
-         "rewards_contract":"",
-         "gaze_staking":"",
-         "btts_lib":"",
+         "access_control":"0x4Ef975d87CC75bB9db79B719F8DBfFBf542f72d9",
+         "gaze_coin": "0xcc5292d5f04948fE682CEDfB8b3C4b4533e81613",
+         "weth":"0x347ecdAd522222eb171814289dbAA334aD270CAf",
+         "lp_token":"0x89236A06a5FAa558F01c74F4fFdbe8FF976afFD9",
+         "rewards_contract":"0x629e712A6d4eA2154B2E2dC077a2E86E79E1550d",
+         "lp_staking":"0xd65Df4760A43Ae54C723123c05ad1302d453fF1a",
+         "btts_lib":"0x7e14941DE3EB1E4b63448194360dce09B06F5c4C",
          "sushi_factory":"0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
     },
      "mainnet":{
@@ -15,7 +15,7 @@ CONTRACTS = {
          "weth":"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
          "lp_token":"",
          "rewards_contract":"",
-         "gaze_staking":"",
+         "lp_staking":"",
          "btts_lib":"",
          "sushi_factory":"0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"
     },
@@ -25,7 +25,7 @@ CONTRACTS = {
          "weth":"",
          "lp_token":"",
          "rewards_contract":"",
-         "gaze_staking":"",
+         "lp_staking":"",
          "btts_lib":"",
          "sushi_factory":"0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
     },
@@ -35,7 +35,7 @@ CONTRACTS = {
          "weth":"",
          "lp_token":"",
          "rewards_contract":"",
-         "gaze_staking":"",
+         "lp_staking":"",
          "btts_lib":"",
          "sushi_factory":"0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
     },
@@ -45,7 +45,7 @@ CONTRACTS = {
          "weth":"",
          "lp_token":"",
          "rewards_contract":"",
-         "gaze_staking":"",
+         "lp_staking":"",
          "btts_lib":"",
          "sushi_factory":"0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
     },
